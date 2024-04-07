19a9c8e70c4c88d7af33f5b7adf2e1e4ef50b616
Karolis <karolissaganas@yahoo.co.uk>
2024-04-07 14:04:12 +0300

    All the media breakingpoints are aligned to my liking

--- JS/script.js
+++ JS/script.js
@@ -6,8 +6,10 @@ function assignClassToElements() {
         image.classList.toggle('showImages'); 
     });
 
+
+    /* It's working fine */
     // Check if the window width is between 744px and 1113px
-    if (window.innerWidth >= 744 && window.innerWidth <= 1113) {
+    if (window.innerWidth >= 730 && window.innerWidth <= 1113) {
         // Selects the class
         var element = document.querySelector('.card--colorful-drawings');
 
@@ -23,7 +25,8 @@ function assignClassToElements() {
             // This makes it so the children stay in the middle
         }
     }
-
+/* This function makes it so when you toggle on the card it centers 
+everything everything inside it(Works globally) */
     var element = document.querySelector('.card--colorful-drawings');
     // Toggle the justify-content property
     if (element.style.justifyContent === 'center') {
@@ -32,23 +35,29 @@ function assignClassToElements() {
         element.style.justifyContent = 'center'; // Apply centering
     }
 
-    // this function makes it so everything works past ėėępix
+
+/* Testing code */
+// if (window.innerWidth >= 443 && window.innerWidth <= 773) {}
+
+
+/*     // this function makes it so everything works past it
     var element = document.querySelector('.card--colorful-drawings');
     var screenWidth = window.innerWidth;
 
     // Define scaling factor and top property based on screen width
-    var scalingFactor, topValue;
+    var scalingFactor, topValue, leftValue;
     if (screenWidth < 773) {
         if (element.style.transform === 'scale(0.6)') {
             scalingFactor = 1;
             topValue = '0px';
         } else {
             scalingFactor = 0.6;
-            topValue = '-300px';
+            topValue = '-300px'
+            leftValue = '0%';
         }
 
         // Apply transform property with the calculated scaling factor
         element.style.transform = 'scale(' + scalingFactor + ')';
         element.style.top = topValue;
-    }
+    } */
 }
