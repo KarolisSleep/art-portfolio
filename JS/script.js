be8cf2a78032300e42327ca13e325cd5b80c4696
Karolis <karolissaganas@yahoo.co.uk>
2024-04-08 16:29:06 +0300

    Simplified the comments in the JS file

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,12 @@
 function assignClassToElements() {
-    /*  Selects all elements with the class .hiddingImages and then
-        toggles the class showImages */
+    /*  Toggles on a class that makes the images appear */
     const images = document.querySelectorAll('.hiddingImages'); 
     images.forEach(function(image) {
         image.classList.toggle('showImages'); 
     });
 
-/* This function makes it so when you toggle on the card it centers 
-everything everything inside it(Works globally) */
+    /* On toggle the card centers 
+    everything inside it(Works globally) */
     var element = document.querySelector('.card--colorful-drawings');
     // Toggle the justify-content property
     if (element.style.justifyContent === 'center') {
