7453897000678a3fe7849e68a9a1185b17303d1f
Karolis <karolissaganas@yahoo.co.uk>
2024-04-08 20:55:28 +0300

    Added different scrollbar colors that would change depending on the hour of the day

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+/* A function for the container */
 function assignClassToElements() {
     /*  Toggles on a class that makes the images appear */
     const images = document.querySelectorAll('.hiddingImages'); 
@@ -15,3 +16,65 @@ function assignClassToElements() {
         element.style.justifyContent = 'center'; // Apply centering
     }
 }
+
+/* A function that changes colors of the scrollbar depending on the hour of the day*/
+function updateTimeBasedScrollbarStyle() {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+
+    let newStyle = "";
+
+    if (currentHour >= 9 && currentHour < 11) {
+        newStyle = `
+            background: rgb(0, 63, 63);
+            -webkit-box-shadow: inset 0 0 6px rgba(0, 255, 255, 0.5);
+        `;
+    } else if (currentHour >= 11 && currentHour < 13) {
+        newStyle = `
+            background: rgb(0, 63, 0);
+            -webkit-box-shadow: inset 0 0 6px rgba(0, 255, 0, 0.5);
+        `;
+    } else if (currentHour >= 13 && currentHour < 14) {
+        newStyle = `
+            background: rgb(63, 63, 0);
+            -webkit-box-shadow: inset 0 0 6px rgba(255, 255, 0, 0.5);
+        `;
+    } else if (currentHour >= 14 && currentHour < 15) {
+        newStyle = `
+            background: rgb(255, 165, 0);
+            -webkit-box-shadow: inset 0 0 6px rgba(255, 255, 0, 0.5);
+        `;
+    } else if (currentHour >= 15 && currentHour < 16) {
+        newStyle = `
+            background: rgb(63, 42, 0);
+            -webkit-box-shadow: inset 0 0 6px rgba(255, 165, 0, 0.5);
+        `;
+    } else if (currentHour >= 16 && currentHour < 18) {
+        newStyle = `
+            background: rgb(63, 0, 0);
+            -webkit-box-shadow: inset 0 0 6px rgba(255, 0, 0, 0.5);
+        `;
+    } else if (currentHour >= 18 && currentHour < 20) {
+        newStyle = `
+            background: rgb(63, 16, 170);
+            -webkit-box-shadow: inset 0 0 6px rgba(160, 32, 240, 0.5);
+        `;
+    } else if (currentHour >= 21 && currentHour < 22) {
+        newStyle = `
+            background: rgb(0, 0, 255);
+            -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 255, 0.5);
+        `;
+    }
+
+    const styleTag = document.getElementById("scrollbar-style");
+
+    if (styleTag) {
+        styleTag.innerHTML = `::-webkit-scrollbar-thumb { ${newStyle} }`;
+    }
+}
+
+// Call the function initially
+updateTimeBasedScrollbarStyle();
+
+// Update every minute
+setInterval(updateTimeBasedScrollbarStyle, 60000);
