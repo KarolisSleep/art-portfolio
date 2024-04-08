92c9420734144b14aa6d1714ded838dadd134ae3
Karolis <karolissaganas@yahoo.co.uk>
2024-04-08 16:14:59 +0300

    Removed unescessary javascript code

--- JS/script.js
+++ JS/script.js
@@ -6,25 +6,6 @@ function assignClassToElements() {
         image.classList.toggle('showImages'); 
     });
 
-
-    /* It's working fine */
-    // Check if the window width is between 744px and 1113px
-    if (window.innerWidth >= 730 && window.innerWidth <= 1113) {
-        // Selects the class
-        var element = document.querySelector('.card--colorful-drawings');
-
-        // Toggle the inline styles
-        if (element.style.position === 'relative') {
-            element.style.position = '';
-            element.style.left = '';
-            element.style.transform = '';
-        } else {
-            element.style.position = 'relative';
-            element.style.left = '50%';
-            element.style.transform = 'translateX(-50%)';
-            // This makes it so the children stay in the middle
-        }
-    }
 /* This function makes it so when you toggle on the card it centers 
 everything everything inside it(Works globally) */
     var element = document.querySelector('.card--colorful-drawings');
